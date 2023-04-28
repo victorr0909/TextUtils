45c52e2187313e451256485b193bd3af994fbd25
victorr0909 <namit09092001@gmail.com>
2023-04-28 21:24:20 +0530

    Added About

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import TextForm from './components/TextForm';
 function App() {
   return (
     <>
-      {/* <Navbar title="TextUtils" /> */}
       <Navbar/>
       <div className="container my-3">
-      {/* <TextForm heading="Enter a Text to analyze below"/> */}
       <TextForm/>
+      <About/>
       </div>
     </>
   );
