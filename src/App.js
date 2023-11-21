a6a82138443995e74be5745a082946c1293faa38
victorr0909 <namit09092001@gmail.com>
2023-11-22 00:03:11 +0530

    I made a change in App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode has been Enabled", "success");
     }
     else{
       setMode('light');
@@ -58,5 +58,5 @@ function App() {
     </> 
   );
 }
-
+//changes12
 export default App;
